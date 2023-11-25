4071ae27127e21d526af4650ad6d6048793b9fc6
dflores24 <dflores@maret.org>
2023-11-25 00:23:43 -0500

    update index.html and index.js
    theoretically done

--- Portfolio Website Project/resources/index.js
+++ Portfolio Website Project/resources/index.js
@@ -18,21 +18,21 @@ const i = document.querySelectorAll(".chickenJoe");
 const j = document.querySelectorAll(".surfsupbottom");
 
 var currColor = '000000';
-var nextColor = colorPalette[Math.random() * colorPalette.length];
+var nextColor = colorPalette[Math.floor(Math.random() * colorPalette.length)];
 
 f.style.color = "#"+nextColor;
 g.style.color = "#"+nextColor;
 h.style.backgroundColor = "#"+nextColor;
 i.style.color = "#"+nextColor;
-j.style.borderTop = "5px solid "+nextColor;
+j.style.borderTop = "5px solid #"+nextColor;
 
 const colorPalette = ['28AFB0', '19647E', 'EE964B', 'E16F7C', '98DFAF', 'FF6666', 'C76D7E', '6B2D5C', '4E6E58', '840032', '187795', '1B4079', '474350', 'DAC4F7', 'C98686', '966B9D', 'F18805', '090C9B' ]
 
 function colorChange() {
   currColor = nextColor;
-  nextColor = colorPalette[Math.random() * colorPalette.length];
+  nextColor = colorPalette[Math.floor(Math.random() * colorPalette.length)];
   while(nextColor == currColor) {
-    nextColor = colorPalette[Math.random() * colorPalette.length];
+    nextColor = colorPalette[Math.floor(Math.random() * colorPalette.length)];
   }
   //currColor stuff
   a.style.backgroundColor = "#"+currColor;
